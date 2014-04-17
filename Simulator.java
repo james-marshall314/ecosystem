5b672916bd7ed67081692627dffc4cfae3706de7
snikolsky <sofia.nikolsky@gmail.com>
2014-04-17 16:10:25 -0400

    Update Simulator.java
    I remade gameGrid as HashMap for easier neighbor finding... you find the neighbor zone by x,y coords, get zone from HashMap by string key x +","+ Y, like:  "0,0" is top left grid square.
    
    In general, I think gameGrid should be a class with its own methods of getNeighbors(), getNumDeer()etc.  Simulator should only runSim and maybe handle user interaction.

--- Simulator.java
+++ Simulator.java
@@ -1,4 +1,9 @@
+import java.util.Random;
+import java.util.List;
+import java.util.Iterator;
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * Simulator creates an runs a simulation of an ecosystem. 
  * The ecosystem is comprised of a list of zones and a list of organisms.
@@ -6,23 +11,47 @@ import java.util.ArrayList;
  * Simulator builds and executes the simulation.
  * Tracks number of turns that have been taken.
  * 
- * @author James Marshall
+ * @author James Marshall, S. Nikoslky
  * @version 0.0
  */
 public class Simulator
 {
-    private ArrayList<Zone> gameGrid;
+    private HashMap<String, Zone> gameGrid;
     private ArrayList<Organism> organisms;
     private int turns;
 
     /**
-     * Initializes gameGrid with 144 instances of Zone. With each addition, 
-     * the param passed to Zone, starting at 0, is incrementented by 1 
-     * so that each Zone has its own number between 0 and 143. 
+     * Initializes gameGrid with 144 instances of Zone. *
+     * each zone gets x and y coords.
      */
     public Simulator()
+    { 
+        Zone tempZone;
+        for (int i=0; i<12; ++i)
+        {
+            for (int j=0; j<12; ++j)
+            {
+                tempZone=new Zone (i, j);
+                gameGrid.put(Integer.toString(i)+","+Integer.toString(j), tempZone);
+            }
+        }
+        
+    }
+    
+    /**
+     * Creates game display 
+     */
+    public void setDisplay()
     {
         
+    }
+    
+     /**
+     * updates game display
+     */  
+    public void updateDisplay()
+    {
+    
     }
 
     /**
@@ -36,16 +65,49 @@ public class Simulator
     /**
      * Looks up a specific zone based on its zone number. 
      */
-    public Zone getZone(int zoneNumber)
+    public Zone getZone(int x, int y)
     {
-        
+        return this.gameGrid.get(Integer.toString(x)+","+Integer.toString(y));
     }
     
     /**
      * Returns a list of adjacent zones based on a given zone number.
      */
-    public ArrayList<Zone> getAdjZones(int zoneNumber)
+    public ArrayList<Zone> getAdjZones(int x, int y)
     {
+        ArrayList<Zone> neighbors = new ArrayList<Zone>();
+
+        int startX=getStartCoord(x);
+        int endX=getEndCoord(x);
+        int startY=getStartCoord(y);
+        int endY=getEndCoord(y);
         
+        for (int i=getStartCoord(x); i<getEndCoord(x); ++i)
+        {
+            for (int j=getStartCoord(y); j<getEndCoord(y); ++j)
+            {
+                if (i!=x || j!=y)
+                    neighbors.add(gameGrid.get (Integer.toString(i)+","+Integer.toString(j)));
+            }
+        }
+        
+        return neighbors;
     }
+    
+    private int getStartCoord(int coord)
+    {
+        if (coord>0)
+            return coord-1;
+        else
+            return coord;
+    }
+    
+    private int getEndCoord(int coord)
+    {
+        if (coord<11)
+            return coord+1;
+        else
+            return coord;
+    }
+
 }
