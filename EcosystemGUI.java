1fbdf771b98ef58ea539791f6288897ccacf85d8
snikolsky <sofia.nikolsky@gmail.com>
2014-04-28 09:30:03 -0400

    Update EcosystemGUI.java

--- EcosystemGUI.java
+++ EcosystemGUI.java
@@ -6,74 +6,93 @@ import java.awt.event.*;
 //import javax.swing.JButton;
 //import javax.swing.JFrame;
 
+/**
+ * EcosystemGUI is the control module for the simulation.  It creates and manages Simulator and 
+ * SimDisplay.  Is controlled by buttonpad.
+ */
 public class EcosystemGUI extends JFrame 
 {
-    // Extends JFrame so it can create frames 
+
+// Extends JFrame so it can create frames
+
     private JButton startButton;
     private JButton exitButton;
     private JButton resetButton;
     private JButton fireButton;
     
     private SimDisplay display;
+    private Simulator sim;
     private Container myPane;
     private JPanel buttonPad;
-    private JPanel buttonPadPanel;
-    private JPanel sim;
     
     public EcosystemGUI() 
     {
 
-        this.setSize(750, 600);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Forest Game");
+    this.setSize(550, 490);
+    this.setResizable(false);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setTitle("Forest Game");
 
+    buttonPad=makeButtonPad();
+    display=new SimDisplay();
+    
+    sim=new Simulator();
+    myPane=getContentPane();
+    myPane.setLayout(new BorderLayout());
+
+    //put buttonPad, simDisplay into main pane
+    myPane.add(buttonPad, BorderLayout.EAST);
+    myPane.add(display, BorderLayout.CENTER);
+    
+    this.setVisible(true);
+}
+   
+ 
+ /**
+  * MakeButtonPad creates a buttonpad, a panel to contain it, and sets action listeners
+  * @return - a buttonPad Jpanel
+  */
+    private JPanel makeButtonPad()
+    {
+        //class-level variables
         startButton = new JButton("Start");
         resetButton = new JButton("Reset");
         exitButton = new JButton("Exit");
         fireButton = new JButton("Fire");
-    
-        display=new SimDisplay();
-        myPane=getContentPane();
-        myPane.setLayout(new BorderLayout());
-    
-        buttonPad=new JPanel();
-        buttonPad.setLayout(new GridLayout(0, 1));
-    
-        //add buttons and button listeners
-        buttonPad.add(startButton);   
-        startButton.addActionListener(new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) {startSim();}
-        });
-        buttonPad.add(resetButton);
-        resetButton.addActionListener(new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) {reset();}
-        });
-        buttonPad.add(fireButton);
-        fireButton.addActionListener(new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) {burn();}
-        });
-        buttonPad.add(exitButton);
-        exitButton.addActionListener(new ActionListener() 
-        {
-            public void actionPerformed(ActionEvent e) {end();}
-        });
         
+        JPanel buttons=new JPanel();
+        buttons.setLayout(new GridLayout(0, 1));
         //put buttonPad into buttonPane so it doesnt stretch in length
-        buttonPadPanel=new JPanel(new FlowLayout());
-        buttonPadPanel.add(buttonPad);
-        //put buttonPadPane into main pane
-        myPane.add(buttonPadPanel, BorderLayout.EAST);
-        myPane.add(display, BorderLayout.CENTER);
-    
-        this.setVisible(true);
+        JPanel buttonPadPanel=new JPanel(new FlowLayout());
+        buttonPadPanel.add(buttons);
+        
+        //add buttons and button listeners
+        buttons.add(startButton);   
+            startButton.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {startSim();}
+            });
+        buttons.add(resetButton);
+            resetButton.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {reset();}
+            });
+        buttons.add(fireButton);
+            fireButton.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {burn();}
+            });
+        buttons.add(exitButton);
+            exitButton.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {end();}
+            });
+            
+        return buttonPadPanel;
     }
-
     private void startSim()
     {
+        
         
     }
     
@@ -84,6 +103,7 @@ public class EcosystemGUI extends JFrame
     
     private void reset()
     {
+        display.clearSim();
         
     }
     
